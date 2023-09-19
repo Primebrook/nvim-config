08cb1c68a96b9006639b75214be197934b0c33c7
Brook <71849503+Primebrook@users.noreply.github.com>
2023-09-19 18:25:54 +0100

    remove <leader>test

--- lua/primebrook/remap.lua
+++ lua/primebrook/remap.lua
@@ -6,23 +6,6 @@ vim.keymap.set("n", "<leader>ff", function()
 	vim.cmd("Telescope find_files")
 end, {})
 
-vim.keymap.set("n", "<leader>test", function()
-	local current_file_path = vim.fn.expand("%")
-	local file_ext = vim.fn.expand("%:e")
-	local test_commands = {
-		exs = "mix test",
-		py = "make test",
-	}
-
-	local test_cmd = test_commands[file_ext] .. " " .. current_file_path
-
-	if test_cmd then
-		vim.cmd("terminal " .. test_cmd)
-	else
-		print("No test command for this file extension")
-	end
-end, {})
-
 -- Copying to system clipboard
 vim.keymap.set("n", "Y", '"+yy', { noremap = true, silent = true })
 vim.keymap.set("v", "Y", '"+y', { noremap = true, silent = true })

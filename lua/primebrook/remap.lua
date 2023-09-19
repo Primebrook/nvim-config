3655188a370b95208bb60cf0c3e80f78ebac5edb
Brook <71849503+Primebrook@users.noreply.github.com>
2023-09-19 18:24:19 +0100

    copy current fp to clipboard

--- lua/primebrook/remap.lua
+++ lua/primebrook/remap.lua
@@ -27,6 +27,12 @@ end, {})
 vim.keymap.set("n", "Y", '"+yy', { noremap = true, silent = true })
 vim.keymap.set("v", "Y", '"+y', { noremap = true, silent = true })
 
+-- Copying current filepath to clipboard
+vim.keymap.set("n", "<leader>fp", function()
+	local filepath = vim.fn.expand("%:p")
+	vim.fn.system("echo " .. filepath .. " | pbcopy")
+end, { noremap = true, silent = true })
+
 -- Managing Window Panes
 vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally

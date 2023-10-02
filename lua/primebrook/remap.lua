aead1905d18ef35846b1229f4efd717888f1da23
Brook <71849503+Primebrook@users.noreply.github.com>
2023-10-02 16:34:04 +0100

    don't error if match isn't found

--- lua/primebrook/remap.lua
+++ lua/primebrook/remap.lua
@@ -42,9 +42,9 @@ vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { norema
 -- json formatting
 function python_dict_to_json()
 	vim.api.nvim_command(":%s/'/\"/g")
-	vim.api.nvim_command(":%s/False/false/g")
-	vim.api.nvim_command(":%s/True/true/g")
-	vim.api.nvim_command(":%s/None/null/g")
+	vim.api.nvim_command(":%s/False/false/ge")
+	vim.api.nvim_command(":%s/True/true/ge")
+	vim.api.nvim_command(":%s/None/null/ge")
 end
 
 vim.keymap.set("n", "<leader>tj", ":lua python_dict_to_json()<CR>", { noremap = true, silent = true }) -- tj == "to json"

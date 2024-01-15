4672619aa19075177fbe36bf6e3219f1bf6031d5
Brook <71849503+Primebrook@users.noreply.github.com>
2024-01-15 17:22:36 +0000

    comment out remap

--- lua/primebrook/remap.lua
+++ lua/primebrook/remap.lua
@@ -88,3 +88,26 @@ vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu, { noremap = true, silent
 
 -- Oil
 vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open current file in system file explorer" })
+
+-- Commenting
+function toggle_comment()
+	-- Get the range of selected lines in visual mode
+	local start_line, end_line = unpack(vim.api.nvim_buf_get_mark(0, "<")), unpack(vim.api.nvim_buf_get_mark(0, ">"))
+
+	-- Process each line in the range
+	for line_num = start_line, end_line do
+		local line = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, false)[1]
+		if line:match("^%s*#") then
+			-- Uncomment the line
+			line = line:gsub("^%s*#", "")
+		else
+			-- Comment the line
+			line = "#" .. line
+		end
+		vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, false, { line })
+	end
+end
+
+-- Map the function to a key in normal and visual mode
+vim.api.nvim_set_keymap("n", "<Leader>c", ":lua toggle_comment()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<Leader>c", ":'<,'>lua toggle_comment()<CR>", { noremap = true, silent = true })

4069a8475e78d7a4105fc33d8bd218e69002d185
Brook <71849503+Primebrook@users.noreply.github.com>
2024-02-16 21:03:53 +0000

    move rust-analyzer and elixirls into the config

--- lua/primebrook/lsp.lua
+++ lua/primebrook/lsp.lua
@@ -4,9 +4,11 @@ function M.setup()
 	local lspconfig = require("lspconfig")
 
 	lspconfig.pyright.setup({})
-	lspconfig.rust_analyzer.setup({})
+	lspconfig.rust_analyzer.setup({
+		cmd = { vim.fn.expand("~/.config/nvim/language_servers/rust-analyzer-aarch64-apple-darwin") },
+	})
 	lspconfig.elixirls.setup({
-		cmd = { vim.fn.expand("~/.bin/elixir-ls/language_server.sh") },
+		cmd = { vim.fn.expand("~/.config/nvim/language_servers/elixir-ls/language_server.sh") },
 	})
 	lspconfig.clangd.setup({
 		cmd = { vim.fn.expand("/Library/Developer/CommandLineTools/usr/bin/clangd") },

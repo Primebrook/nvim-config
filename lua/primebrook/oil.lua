37f8b128d384cc300f65dc0e5e9742a5137d3a02
Brook <71849503+Primebrook@users.noreply.github.com>
2024-02-17 15:26:16 +0000

    get rid of default keymaps (#16)

--- lua/primebrook/oil.lua
+++ lua/primebrook/oil.lua
@@ -1,7 +1,19 @@
 local M = {}
 
 function M.setup()
-	require("oil").setup()
+	require("oil").setup({
+		use_default_keymaps = false,
+		keymaps = {
+			["<CR>"] = "actions.select",
+			["-"] = "actions.parent",
+		},
+		view_options = {
+			show_hidden = false,
+			is_hidden_file = function(name, bufnr)
+				return vim.startswith(name, "..") or name == ".git" or name == ".vscode"
+			end,
+		},
+	})
 end
 
 return M

4e88d27abfe46816c55f426e6eeed0e52fd22c95
Brook <71849503+Primebrook@users.noreply.github.com>
2024-02-22 19:11:45 +0000

    typo

--- scripts/open_in_github.sh
+++ scripts/open_in_github.sh
@@ -15,7 +15,7 @@ REPO_URL=$(gh repo view --json url -q .url)
 # Get the current branch name
 BRANCH_NAME=$(git branch --show-current)
 
-# Get the relative filepath: not this line assumes you're running 
+# Get the relative filepath: note this line assumes you're running
 # this script from anywhere within the git repo (doesn't have to be the root).
 RELATIVE_FILEPATH=$(git ls-files --full-name $FILEPATH)
 

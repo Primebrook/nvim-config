7c5601198a5856eb984b1800b01a641f3f331cd4
Brook <71849503+Primebrook@users.noreply.github.com>
2024-02-22 19:30:03 +0000

    add line number option

--- scripts/open_in_github.sh
+++ scripts/open_in_github.sh
@@ -3,9 +3,12 @@
 # Filepath is the first argument to the script
 FILEPATH="$1"
 
-# Ensure a file path is provided
+# Line number is the second argument to the script
+LINE_NUMBER="$2"
+
+# Ensure a filepath is provided
 if [ -z "$FILEPATH" ]; then
-  echo "Please provide a file path"
+  echo "Usage: open_in_github.sh <filepath> optional(<line_number>)"
   exit 1
 fi
 
@@ -19,6 +22,12 @@ BRANCH_NAME=$(git branch --show-current)
 # this script from anywhere within the git repo (doesn't have to be the root).
 RELATIVE_FILEPATH=$(git ls-files --full-name $FILEPATH)
 
-URL="$REPO_URL/blob/$BRANCH_NAME/$RELATIVE_FILEPATH"
+# Construct the URL
+if [ -z "$LINE_NUMBER" ]; then
+  URL="$REPO_URL/blob/$BRANCH_NAME/$RELATIVE_FILEPATH"
+else
+  URL="$REPO_URL/blob/$BRANCH_NAME/$RELATIVE_FILEPATH#L$LINE_NUMBER"
+fi
 
+echo "Opening $URL"
 open $URL

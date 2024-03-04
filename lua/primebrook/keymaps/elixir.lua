e2e2d179e261590b1fbcff4b29d0410750357042
Brook <71849503+Primebrook@users.noreply.github.com>
2024-03-04 18:04:30 +0000

    keymap for inserting IEx.pry

--- lua/primebrook/keymaps/elixir.lua
+++ lua/primebrook/keymaps/elixir.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
 	-- Prefix w/ pipeline: current line
-	vim.keymap.set("n", "<leader>pp", "I|> ", { noremap = true, silent = true, buffer = true })
+	vim.keymap.set("n", "<leader>pp", "I|> <Esc>", { noremap = true, silent = true, buffer = true })
 	-- Prefix w/ pipeline: visual selection
 	vim.keymap.set("v", "<leader>pp", ":'<,'>norm I|> <Esc>", { noremap = true, silent = true, buffer = true })
 
@@ -13,6 +13,10 @@ function M.setup()
 
 	-- Mix format
 	vim.keymap.set("n", "<leader>ff", ":!mix format<CR>", { noremap = true, silent = true, buffer = true })
+
+	-- Add binding
+	vim.keymap.set("n", "<leader>x", "orequire IEx ; IEx.pry<Esc>", { noremap = true, silent = true, buffer = true })
+	vim.keymap.set("n", "<leader>X", "Orequire IEx ; IEx.pry<Esc>", { noremap = true, silent = true, buffer = true })
 end
 
 return M

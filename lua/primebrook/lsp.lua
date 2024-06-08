f7748ab8eb574b501b1bd1eb9df64695b686d557
Brook <71849503+Primebrook@users.noreply.github.com>
2024-06-08 02:14:49 +0100

    Typescript Setup (ts/js/tsx/jsx) (#23)
    * tsserver
    
    * formatting keymap

--- lua/primebrook/lsp.lua
+++ lua/primebrook/lsp.lua
@@ -10,6 +10,16 @@ function M.setup()
 	lspconfig.clangd.setup({
 		cmd = { vim.fn.expand("/Library/Developer/CommandLineTools/usr/bin/clangd") },
 	})
+
+	-- TypeScript and JavaScript LSP
+	lspconfig.tsserver.setup({
+		on_attach = function(client)
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+		end,
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+		cmd = { "typescript-language-server", "--stdio" },
+	})
 end
 
 return M

7799f4f9d2bb73db042250cf084673d2a31917ff
Brook <71849503+Primebrook@users.noreply.github.com>
2025-02-17 11:40:57 +0000

    pylsp

--- lua/primebrook/lsp.lua
+++ lua/primebrook/lsp.lua
@@ -4,17 +4,24 @@ function M.setup()
 	local lspconfig = require("lspconfig")
 	vim.lsp.set_log_level("debug")
 
-	lspconfig.pyright.setup({
+	lspconfig.pylsp.setup({
+		cmd = { vim.fn.expand("~/.local/bin/pylsp") },
 		settings = {
-			python = {
-				pythonPath = "/Users/brook/.pyenv/versions/garmin/bin/python",
-				analysis = {
-					autoSearchPaths = true,
-					useLibraryCodeForTypes = true,
+			pylsp = {
+				plugins = {
+					jedi_hover = { enabled = false }, -- Disable hover if not needed
+					jedi_signature_help = { enabled = false }, -- Disable signature help
+					jedi_symbols = { enabled = false }, -- Disable workspace symbols
+					pylint = { enabled = false }, -- Turn off Pylint if using Ruff
+					pyflakes = { enabled = false },
+					pycodestyle = { enabled = false },
+					ruff = { enabled = true }, -- Use Ruff instead (fast)
+					yapf = { enabled = false },
 				},
 			},
 		},
 	})
+
 	lspconfig.elixirls.setup({
 		cmd = { vim.fn.expand("~/.elixir-ls/language_server.sh") },
 		settings = {
@@ -27,17 +34,6 @@ function M.setup()
 	lspconfig.clangd.setup({
 		cmd = { vim.fn.expand("/Library/Developer/CommandLineTools/usr/bin/clangd") },
 	})
-
-	-- TypeScript and JavaScript LSP
-	-- tsserver IS DEPRECATED USE ts_ls instead
-	--	lspconfig.tsserver.setup({
-	--		on_attach = function(client)
-	--			client.resolved_capabilities.document_formatting = false
-	--			client.resolved_capabilities.document_range_formatting = false
-	--		end,
-	--		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-	--		cmd = { "typescript-language-server", "--stdio" },
-	--	})
 
 	-- Marksman LSP for Markdown
 	lspconfig.marksman.setup({

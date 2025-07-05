4f5454d3f02c3a33fd326893c374db7b31153727
Brook <71849503+Primebrook@users.noreply.github.com>
2025-07-05 10:17:52 +0100

    inline latex for personal blog

--- lua/primebrook/keymaps/markdown.lua
+++ lua/primebrook/keymaps/markdown.lua
@@ -3,6 +3,9 @@ local M = {}
 function M.setup()
 	-- Markdown Preview
 	vim.keymap.set("n", "<leader>p", ":MarkdownPreview<CR>", { noremap = true, silent = true })
+
+	-- inline latex (for personal blog)
+	vim.keymap.set("n", "<leader>il", [[a{% include inline.html eq="" %}<Esc>F"]], { noremap = true, silent = true })
 end
 
 return M
